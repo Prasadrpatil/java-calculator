6ce8aa3798532b75267c03efc4521e51e4856683
Prasad Patil <68043685+Prasadrpatil@users.noreply.github.com>
2023-03-16 11:16:48 +0530

    Update s3-state.tf

--- terraform/s3-state.tf
+++ terraform/s3-state.tf
@@ -1,12 +1,12 @@
 resource "aws_s3_bucket" "terraform_backend" {
-  bucket = "devsecops-poc-terrafrom-state"
-  acl    = "private"   
+  bucket = "devsecops-poc-terrafrom-state"
+  acl    = "private"
 } 
 
 terraform {
-  backend "s3" {
-    bucket = aws_s3_bucket.terraform_backend.id
-    key    = "terraform/terraform.tfstate"
-    region = "us-east-1"
-  }
+   backend "s3" {
+    bucket = aws_s3_bucket.terraform_backend.id
+    key    = "terraform/terraform.tfstate"
+    region = "us-east-1"
+  }
 }
