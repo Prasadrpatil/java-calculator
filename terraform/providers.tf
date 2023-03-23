eb0674d03c25e800a162da57e09d8554850d211b
Prasad Patil <68043685+Prasadrpatil@users.noreply.github.com>
2023-03-23 14:14:55 +0530

    Update providers.tf

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -6,12 +6,7 @@ provider "aws" {
   region = var.aws_region
 }
 
-data "aws_availability_zone" "example" {
-  state = "available"
-  name  = "us-east-1"
-}
-
-#data "aws_availability_zones" "available" {}
+data "aws_availability_zones" "available" {}
 
 # Not required: currently used in conjunction with using
 # icanhazip.com to determine local workstation external IP
